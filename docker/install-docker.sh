b0f8d13f012ad3354d5211651ee96b20e53aa558
Meer Sagor <meersagor.contact@gmail.com>
2024-10-07 11:12:30 +0600

    feat: docker command permission

--- docker/install-docker.sh
+++ docker/install-docker.sh
@@ -9,14 +9,23 @@ check_docker_installed() {
 }
 
 install_docker() {
+    sudo apt update
+    sudo apt install -y docker.io
 
-    sudo apt install docker.io
+    # Add the current user to the docker group to avoid permission issues
+    sudo usermod -aG docker "$USER"
+
+    # Restart Docker service to apply the changes
+    sudo systemctl enable docker
+    sudo systemctl start docker
+
+    # Inform the user to log out and log back in
+    echo "Docker has been installed. Please log out and log back in to apply group changes."
 
     exit 0
 }
-
 if check_docker_installed; then
-    echo "docker is already installed."
+    echo "Docker is already installed."
 else
     install_docker
 fi
