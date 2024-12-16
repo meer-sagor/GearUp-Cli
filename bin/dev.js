1574ceb2d538b0340ce5261b8f47fefe86380bf9
Meer Sagor <meersagor.contact@gmail.com>
2024-12-16 14:17:06 +0600

    fix: bin/dev.js script for development

--- bin/dev.js
+++ bin/dev.js
@@ -1,6 +1,12 @@
-#!/usr/bin/env -S node --loader ts-node/esm --disable-warning=ExperimentalWarning
+#!/usr/bin/env node
 
 // eslint-disable-next-line n/shebang
 import {execute} from '@oclif/core'
+import { register } from 'node:module';
+import { pathToFileURL } from 'node:url';
 
-await execute({development: true, dir: import.meta.url})
+// Register the ts-node/esm loader
+register('ts-node/esm', pathToFileURL('./'));
+
+// Execute the CLI
+await execute({ development: true, dir: import.meta.url });
